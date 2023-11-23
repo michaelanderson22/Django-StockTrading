d2995e968324d0132f8921cf5dcd88848568a100
michaelanderson22 <michaelanderson22@mail.weber.edu>
2023-11-23 16:16:02 -0700

    Checkpoint 6, Stocks should be updated appropriately, once per day

--- FinalProject/mysite/views.py
+++ FinalProject/mysite/views.py
@@ -11,15 +11,18 @@ from datetime import datetime
 
 def HomePage(request):
     api_key = settings.ALPHA_VANTAGE_API_KEY
-    symbols = ['AAPL', 'GOOGL', 'MSFT', 'AMZN', 'GOOGL', 'NVDA', 'META', 'TSLA', 'LLY', 'TSM', 'WMT', 'JNJ', 'ORCL', 'HD', 'ADBE', 'COST', 'KO', 'TM', 'BAC', 'PEP']  # Add more stock symbols as needed
+    symbols = ['AAPL', 'GOOGL', 'MSFT', 'AMZN', 'GOOGL', 'NVDA', 'META', 'TSLA', 'LLY', 'TSM', 'WMT', 'JNJ', 'ORCL',
+               'HD', 'ADBE', 'COST', 'KO', 'TM', 'BAC', 'PEP']  # Add more stock symbols as needed
 
     for symbol in symbols:
         try:
             # Check the last update timestamp for the stock
             last_update_time = Stock.objects.filter(symbol=symbol).order_by('-last_updated').first()
 
-            # Only make an API call if it's been more than a day since the last update or no previous update exists
-            if not last_update_time or (timezone.now() - last_update_time.last_updated).days > 0:
+            # Only make an API call if it's been more than a day since the last update and you have responses left
+            if last_update_time and (timezone.now() - last_update_time.last_updated).days > 0:
+                print((timezone.now() - last_update_time.last_updated).days)
+
                 url = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={api_key}'
                 response = requests.get(url)
                 response.raise_for_status()  # Raise an exception for bad responses
@@ -28,30 +31,27 @@ def HomePage(request):
                 print(data)
                 time_series = data.get('Global Quote', {})
 
-                # Extracting the date from the response
-                date = time_series.get('07. latest trading day', None)
+                # Check if a record with the same symbol already exists
+                existing_stock = Stock.objects.filter(symbol=symbol).first()
 
-                if date:
-                    # Check if a record with the same symbol and date already exists
-                    existing_stock = Stock.objects.filter(symbol=symbol, last_updated=date).first()
-
-                    if existing_stock:
-                        # Update existing record
-                        existing_stock.current_price = float(time_series.get('05. price', 0))
-                        existing_stock.change_percent = time_series.get('10. change percent',
-                                                                        '0%')  # Default to '0%' if not provided
-                        existing_stock.last_updated = timezone.make_aware(datetime.strptime(date, '%Y-%m-%d'))
-                        existing_stock.save()
-                    else:
-                        # Create a new record
-                        stock = Stock(
-                            symbol=symbol,
-                            current_price=float(time_series.get('05. price', 0)),
-                            change_percent=time_series.get('10. change percent', '0%'),
-                            # Default to '0%' if not provided
-                            last_updated=timezone.make_aware(datetime.strptime(date, '%Y-%m-%d')),
-                        )
-                        stock.save()
+                # Check if it's been more than a day since the last update
+                if existing_stock and (timezone.now() - existing_stock.last_updated).days > 0:
+                    # Update existing record
+                    existing_stock.current_price = float(time_series.get('05. price', 0))
+                    existing_stock.change_percent = time_series.get('10. change percent', '0%')
+                    # Default to '0%' if not provided
+                    existing_stock.last_updated = timezone.now()
+                    existing_stock.save()
+                elif not existing_stock:
+                    # Create a new record
+                    stock = Stock(
+                        symbol=symbol,
+                        current_price=float(time_series.get('05. price', 0)),
+                        change_percent=time_series.get('10. change percent', '0%'),
+                        # Default to '0%' if not provided
+                        last_updated=timezone.now(),
+                    )
+                    stock.save()
 
         except HTTPError as e:
             print(f"Error fetching data for {symbol}: {e}")
